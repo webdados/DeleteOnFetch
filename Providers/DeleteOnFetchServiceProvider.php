ecc5fa4dacd9445d39dbeae7b06a212967043680
Marco Almeida <marcoalmeida@webdados.pt>
2023-07-05 12:00:14 +0100

    2.0 - There's now an option to activate "Delete messages after fetch" in each Mailbox > Connection Settings > Fetching Emails - Introduce this CHANGELOG.md file

--- Providers/DeleteOnFetchServiceProvider.php
+++ Providers/DeleteOnFetchServiceProvider.php
@@ -36,10 +36,10 @@ class DeleteOnFetchServiceProvider extends ServiceProvider
     public function hooks()
     {
         // Delete from server after set as seen / fetched
-        \Eventy::addAction('fetch_emails.after_set_seen', function( $message, $mailbox, $fetchemailsobject ) {
-            // Delete without warning
-            $message->delete();
-        }, 20, 3 );
+        \Eventy::addAction( 'fetch_emails.after_set_seen', array( $this, 'deleteMessage' ), 20, 3 );
+        // Option on the Mailbox Fetching Emails options
+        \Eventy::addAction( 'mailbox.connection_incoming.after_default_settings', array( $this, 'mailboxIncomingOptions' ) );
+        \Eventy::addAction( 'mailbox.incoming_settings_before_save', array( $this, 'mailboxIncomingOptionsSave' ), 10, 2 );
     }
 
     /**
@@ -116,5 +116,55 @@ class DeleteOnFetchServiceProvider extends ServiceProvider
     public function provides()
     {
         return [];
+    }
+
+    /**
+     * Delete the message from remote server.
+     *
+     * @return void
+     */
+    public function deleteMessage( $message, $mailbox, $fetchemailsobject ) {
+        if ( \Option::get( 'deleteonfetch.delete_after_fetch_active_mailbox_'.$mailbox->id ) == '1' ) {
+            // Delete without warning
+            $message->delete();
+        }
+    }
+
+    /**
+     * Option on the Mailbox Fetching Emails option.
+     *
+     * @return void
+     */
+    public function mailboxIncomingOptions( $mailbox ) {
+        ?>
+        <div id="delete-on-fetch-options">
+            <div class="form-group">
+                <label for="delete_after_fetch_active" class="col-sm-2 control-label"><?php echo __( 'Delete messages after fetch' ); ?></label>
+                <div class="col-sm-6">
+                    <!--<input id="delete_after_fetch_active" name="delete_after_fetch_active" type="checkbox">
+                    <div class="form-help"><?php echo __( 'Delete all incoming messages from the remote server after fetching them. Use at your own risk.' ); ?></div>-->
+                    <div class="controls">
+                        <div class="onoffswitch-wrap">
+                            <div class="onoffswitch">
+                                <input type="checkbox" name="delete_after_fetch_active" value="1" id="delete_after_fetch_active" class="onoffswitch-checkbox" <?php if ( \Option::get( 'deleteonfetch.delete_after_fetch_active_mailbox_'.$mailbox->id ) == '1' ) echo 'checked="checked"'; ?>>
+                                <label class="onoffswitch-label" for="delete_after_fetch_active"></label>
+                            </div>
+                            <i class="glyphicon glyphicon-info-sign icon-info icon-info-inline" data-toggle="popover" data-trigger="hover" data-placement="top" data-content="<?php echo __( 'Delete all incoming messages from the remote server after fetching them. Use at your own risk.' ); ?>"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+        </div>
+        <?php
+    }
+
+    /**
+     * Save option on the Mailbox Fetching Emails option.
+     *
+     * @return void
+     */
+    public function mailboxIncomingOptionsSave( $mailbox, $request ) {
+        \Option::set( 'deleteonfetch.delete_after_fetch_active_mailbox_'.$mailbox->id, ( $request->filled( 'delete_after_fetch_active' ) ? '1' : '0' ) );
     }
 }
--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Delete On Fetch
-Freescout module that deletes emails from remote server, after fetched, without warnings.
+Freescout module that deletes emails from the remote server, after fetched, without warnings.
 Use at your own risk!
 
-PR are welcome.
+PRs are welcome.
 
 ## Installation
-* Download latest version from this repository
+* Download the latest version from this repository
 * Upload/extract to the Modules folder on your Freescout install, inside a folder named DeleteOnFetch
 * Go to Manage > Settings > Tools and Clear Cache
 * Go to Modules and activate "Delete On Fetch"
+* For each Mailbox you want to delete the emails after fetching, go to Connection Settings > Fetching Emails and activate "Delete messages after fetch"
 
 ## To do
-* Option on each mailbox to activate deletion on fetch, or not
+* Option to only delete from server after some days
